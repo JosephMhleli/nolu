1927308fa4af0123da708b357ee50eab02db9cec
LMMayila <lihle@mayilaindustries.com>
2023-11-14 10:05:36 +0200

    changes

--- Javascript/topnav2.js
+++ Javascript/topnav2.js
@@ -5,27 +5,289 @@ function showCategory(category) {
       case 1:
         // Check if the category is ROMANCE
         if (category === 1) {
-          content = '<h2>Category 1</h2><div class="tobchosen"><p>Lihle</p></div>';
-        } else {
-          content = '<h2>Category 1</h2><div class="tobchosen"><a href="#ews">ROMANCE</a><a href="#ews">CHILDREN</a><a href="#ews">FICTION</a><a href="#ews">NON-FICTION</a></div>';
-        }
+          content += '<div class="flex-container">';
+
+  content += '<div class="flex-item">';
+  content += '<h3>Romance</h3>';
+  content += '<div class="tobchosen">';
+  content += '<div class="flex-column">';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+  
+  content += '</div>';
+  content += '</div>';
+  content += '</div>';
+
+  content += '<div class="flex-item">';
+  content += '<h3>Erotic Fiction</h3>';
+  content += '<div class="tobchosen">';
+  content += '<div class="flex-column">';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">BDSM Romanc</a></div>';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Forbidden Love Affairs</a></div>';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">LGBTQ+ Romance</a></div>';
+ 
+  content += '</div>';
+  content += '</div>';
+  content += '</div>';
+
+  content += '<div class="flex-item">';
+  content += '<h3>Adult Romance</h3>';
+  content += '<div class="tobchosen">';
+  content += '<div class="flex-column">';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Second Chance Romance</a></div>';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Small Town Romance</a></div>';
+  content += '<div class="flex-item"><a href="/Pages/bookspage.html">Romantic Comedy</a></div>';
+  
+  content += '</div>';
+  content += '</div>';
+  content += '</div>';
+
+  content += '</div>'; // Close flex-container
+        } 
         break;
+
       case 2:
-        content = '<h2>Category 2</h2><div class="tobchosen"><a href="#ews">ROMANCE</a><a href="#ews">CHILDREN</a><a href="#ews">FICTION</a><a href="#ews">NON-FICTION</a></div>';
+        if(category == 2)
+        {
+         
+          content += '<div class="flex-container">';
+
+          content += '<div class="flex-item">';
+          content += '<h3>Children Fiction</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Magical Adventures</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Animal Tales</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Mystery and Adventure</a></div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Children Non- Fiction</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Biographies for Kids</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Science and Nature Exploration</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">History for young minds</a></div>';
+          
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Language ad Literature</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Tales for Young Readers</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Multilingual Storybooks</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Folktales and Fairy Tales</a></div>';
+          
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '</div>'; // Close flex-container
+        }
+       
         break;
       case 3:
-        content = '<h2>Category 3</h2><div class="tobchosen"><a href="#ews">ROMANCE</a><a href="#ews">CHILDREN</a><a href="#ews">FICTION</a><a href="#ews">NON-FICTION</a></div>';
+        if(category == 3)
+        {
+         
+          content += '<div class="flex-container">';
+
+          content += '<div class="flex-item">';
+          content += '<h3> LOCAL-FICTION</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Regional Setting Novels</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Fiction</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Urban Fiction</a></div>';
+        
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Crime and Thrillers</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Police Procedurals</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Psychological Thrillers</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Legal Thrillers</a></div>';
+         
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Science Fiction & Fantasy</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Space Opera</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">High Fantasy</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Science Fiction Thrillers</a></div>';
+        
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '</div>'; // Close flex-container
+        }
+       
+        break;
+        case 4:
+        if(category == 4)
+        {
+         
+          content += '<div class="flex-container">';
+
+          content += '<div class="flex-item">';
+          content += '<h3>Biographies</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Political Biographies</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Scientific Biographies</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Literature Biographies</a></div>';
+       
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Art & Design</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Art History</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Graphic Design</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Architecture and Design</a></div>';
+      
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Computing & Technology</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Programming and Software Development</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Cybersecurity and Ethical Hacking</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Technology and Society</a></div>';
+        
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '</div>'; // Close flex-container
+        }
+       
+        break;
+        case 5:
+        if(category == 5)
+        {
+         
+          content += '<div class="flex-container">';
+
+          content += '<div class="flex-item">';
+          content += '<h3>Latest Releases and BestSellers</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+          content += '<div class="flex-item">Book 4</div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Book of the Month</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+          content += '<div class="flex-item">Book D</div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Book Reviews and Recommendations</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+          content += '<div class="flex-item">Book W</div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '</div>'; // Close flex-container
+        }
+       
+        break;
+        case 6:
+        if(category == 6)
+        {
+         
+          content += '<div class="flex-container">';
+
+          content += '<div class="flex-item">';
+          content += '<h3>Historical Romance</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+          content += '<div class="flex-item">Book 4</div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Erotic Fiction</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+          content += '<div class="flex-item">Book D</div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '<div class="flex-item">';
+          content += '<h3>Adult Romance</h3>';
+          content += '<div class="tobchosen">';
+          content += '<div class="flex-column">';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Historical Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Contemporary Romance</a></div>';
+          content += '<div class="flex-item"><a href="/Pages/bookspage.html">Paranormal Romance</a></div>';
+          content += '<div class="flex-item">Book W</div>';
+          content += '</div>';
+          content += '</div>';
+          content += '</div>';
+        
+          content += '</div>'; // Close flex-container
+        }
+       
         break;
       // Add more cases as needed
 
       default:
         content = '';
     }
-
+   
     document.getElementById('hoverContent').innerHTML = content;
     document.querySelector('.hoverdiv').style.display = 'block';
   }
-
+  function showHoverDiv() {
+    document.querySelector('.hoverdiv').style.display = 'block';
+  }
   function hideHoverDiv() {
     document.querySelector('.hoverdiv').style.display = 'none';
   }

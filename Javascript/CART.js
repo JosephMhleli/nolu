099059c4a4053c3069199c4c6a8fa6beec887e40
LMMayila <lihle@mayilaindustries.com>
2023-11-17 13:57:04 +0200

    changes

--- Javascript/CART.js
+++ Javascript/CART.js
@@ -1,7 +1,7 @@
 function redirectToProductPage(title, author, imagePath,price) {
     
     const queryString = `?title=${encodeURIComponent(title)}&author=${encodeURIComponent(author)}&image=${encodeURIComponent(imagePath)}&price=${encodeURIComponent(price)}`;
-    window.location.href = "Pages/ProductPage.html" + queryString;
+    window.location.href = "ProductPage.html" + queryString;
 }
 
   

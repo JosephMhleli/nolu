8ee6feb3106fa92b8d8e3d4399a400695b4867d5
LMMayila <lihle@mayilaindustries.com>
2023-11-20 12:24:37 +0200

    changes

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html' )
 
+@app.route('/login')
+def login():
+    return render_template('LoginPage.html')
+
+@app.route('/register')
+def register():
+    return render_template('Register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

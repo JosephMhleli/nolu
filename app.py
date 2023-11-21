6a69c3b32c5c4496aa7ea2e7d56d02a82bf8efe7
LMMayila <lihle@mayilaindustries.com>
2023-11-21 12:28:26 +0200

    chnagesforbook and user

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from passlib.hash import bcrypt
 
 from datetime import datetime,timedelta,timezone
 from functools import wraps
-from db import User, Session
+from db import User, UserSession
+from db import Book, BookSession
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c87793cf26c24a759e75c3713de80466'    
 
@@ -37,7 +39,7 @@ def login():
     try:
         email = request.form['email']
         password = request.form['password']
-        db_session = Session()
+        db_session = UserSession()
         user = db_session.query(User).filter_by(email=email).first()
 
         if user and bcrypt.verify(password, user.password):
@@ -65,7 +67,7 @@ def register():
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
-        db_session = Session()
+        db_session = UserSession()
         if password != confirm_password:
             return make_response('Passwords do not match. Please try again.', 400)
         
@@ -94,7 +96,110 @@ def logout():
 def myBook():
      return 'View Book'
 
+@app.route('/BookInte', methods=['GET'])
+def get_books():
+    try:
+        # Query the database to get all books
+        dbb_session = BookSession()
+        books = dbb_session.query(Book).all()
+        dbb_session.close()
 
+        # Prepare the response data
+        book_list = [{"id": book.id, "title": book.title, "author": book.author, "category": book.category, "price": book.price, "image_url": book.image_url} for book in books]
+
+        # Return the list of books as JSON
+        return jsonify({"books": book_list})
+    except Exception as e:
+        return make_response(str(e), 500)
+   
+@app.route('/BookInte', methods=['POST'])
+def add_book():
+    try:
+        # Extract book information from the request form
+        title = request.form['title']
+        author = request.form['author']
+        category = request.form['category']
+        img_url = request.form['img_url']
+        price = request.form['price']
+
+        if not all([title, author, category, img_url, price]):
+          abort(400, 'Missing required fields')
+        # Create a new book instance
+        new_book = Book(title=title, author=author, category=category, image_url=img_url, price=price)
+
+        # Add the book to the database
+        dbb_session = BookSession()
+        dbb_session.add(new_book)
+        dbb_session.commit()
+        dbb_session.close()
+
+        return 'Book added successfully'
+    except IntegrityError:
+        dbb_session.rollback()
+        return make_response('Book with the same title already exists', 409)
+    except Exception as e:
+        return make_response(str(e), 500)
+ 
+ 
+@app.route('/BookInte', methods=['PUT'])
+def update_book():
+    try:
+        # Extract book information from the request form
+        book_id = request.form['id']
+        title = request.form['title']
+        author = request.form['author']
+        category = request.form['category']
+        img_url = request.form['img_url']
+        price = request.form['price']
+
+        if not all([book_id, title, author, category, img_url, price]):
+            abort(400, 'Missing required fields')
+
+        # Query the database to get the existing book
+        db_session = BookSession()
+        existing_book = db_session.query(Book).filter_by(id=book_id).first()
+
+        # Update the book information
+        existing_book.title = title
+        existing_book.author = author
+        existing_book.category = category
+        existing_book.image_url = img_url
+        existing_book.price = price
+
+        # Commit the changes to the database
+        db_session.commit()
+        db_session.close()
+
+        return 'Book updated successfully'
+    except Exception as e:
+        return make_response(str(e), 500)
+    
+@app.route('/BookInte', methods=['DELETE'])
+def delete_book():
+    try:
+        # Extract book ID from the request form
+        book_id = request.form['id']
+         
+        if not book_id:
+            abort(400, 'Missing book ID')
+        # Query the database to get the existing book
+        db_session = BookSession()
+        book_to_delete = db_session.query(Book).filter_by(id=book_id).first()
+
+        # Remove the book from the database
+        db_session.delete(book_to_delete)
+        db_session.commit()
+        db_session.close()
+
+        return 'Book deleted successfully'
+    except Exception as e:
+        return make_response(str(e), 500)
+           
+    
+     
+    
+            
+    
 # @app.route('/Add2Cart',methods = ['POST','GET','DELETE'])
 # @token_required
 # def addtocart():
--- db.py
+++ db.py
@@ -5,20 +5,33 @@ from passlib.hash import bcrypt
 
 Base = declarative_base()
 
+# User Class
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    name = Column(String(50), unique=False,nullable=False)
-    surname = Column(String(50), unique=False,nullable=False)
-    email = Column(String(50), unique=True,nullable=False)
+    name = Column(String(50), unique=False, nullable=False)
+    surname = Column(String(50), unique=False, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
     password = Column(String(100))
-    
 
-engine = create_engine('sqlite:///NoluUsers.db', echo=True)
+# User Database
+user_engine = create_engine('sqlite:///NoluUsers.db', echo=True)
+Base.metadata.create_all(user_engine)
+UserSession = sessionmaker(bind=user_engine)
 
-# Create the table
-Base.metadata.create_all(engine)
+# Book Class
+class Book(Base):
+    __tablename__ = 'books'
 
-# Create a Session
-Session = sessionmaker(bind=engine)
+    id = Column(Integer, Sequence('book_id_seq'), primary_key=True)
+    title = Column(String(100), unique=False, nullable=False)
+    author = Column(String(100), unique=False, nullable=False)
+    category = Column(String(50))
+    price = Column(String(20))
+    image_url = Column(String(100))
+
+# Book Database
+book_engine = create_engine('sqlite:///BooksDatabase.db', echo=True)
+Base.metadata.create_all(book_engine)
+BookSession = sessionmaker(bind=book_engine)
